dde5a351c55e7e67de4dc0bd691dad08df44f030
Xavier Villavicencio <jxavier2525@gmail.com>
2024-11-18 09:46:49 -0500

    Mod tags and mod materials

--- neoforge/src/main/java/com/graclyxz/manymoreoresandcrafts/ManyMoreOresAndCraftsMod.java
+++ neoforge/src/main/java/com/graclyxz/manymoreoresandcrafts/ManyMoreOresAndCraftsMod.java
@@ -12,17 +12,9 @@ import static com.graclyxz.manymoreoresandcrafts.Constants.MOD_ID;
 @Mod(MOD_ID)
 public class ManyMoreOresAndCraftsMod {
 	public ManyMoreOresAndCraftsMod(IEventBus modEventBus) {
-
 		ManyMoreOresAndCraftsModBlocks.init(modEventBus);
 		ManyMoreOresAndCraftsModItems.init(modEventBus);
 		ManyMoreOresAndCraftsModTabs.init(modEventBus);
-
-		/*ManyMoreOresAndCraftsModBlocks.REGISTRY.register(modEventBus);
-
-		ManyMoreOresAndCraftsModItems.REGISTRY.register(modEventBus);
-
-		ManyMoreOresAndCraftsModTabs.REGISTRY.register(modEventBus);*/
-
 		CommonClass.init();
 	}
 }
