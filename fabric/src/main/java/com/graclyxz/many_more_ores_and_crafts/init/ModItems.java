ec04ede4ab8f3885208da9560eb0d42c9ae1b321
Xavier Villavicencio <jxavier2525@gmail.com>
2025-01-06 20:58:21 -0500

    balanced all swords

--- fabric/src/main/java/com/graclyxz/many_more_ores_and_crafts/init/ModItems.java
+++ fabric/src/main/java/com/graclyxz/many_more_ores_and_crafts/init/ModItems.java
@@ -42,9 +42,9 @@ public class ModItems {
        5. Item propeties */
 
     public static final List<Item> ADAMANTITE_ITEMS = registerAllItems("adamantite", Tool.ADAMANTITE, Armor.ADAMANTITE,
-            new float[]{5, -1.8f}, new float[]{4f, -2.8f}, new float[]{8f, -3f}, new float[]{0f, 0f}, new float[]{4.5f, -3f},
+            new float[]{7.5f, -2f}, new float[]{4f, -2.8f}, new float[]{8f, -3f}, new float[]{0f, 0f}, new float[]{4.5f, -3f},
             new Item.Properties().rarity(Rarity.RARE));
-    public static final List<Block> ADAMANTITE_BLOCKS = registerAllBlocks("adamantite",  new float[]{8f, 10f}, SoundType.STONE,
+    public static final List<Block> ADAMANTITE_BLOCKS = registerAllBlocks("adamantite",  new float[]{8f, 10f}, SoundType.DEEPSLATE,
             BlockBehaviour.Properties.of(),
             new Item.Properties().rarity(Rarity.RARE));
 
@@ -52,56 +52,56 @@ public class ModItems {
     public static final List<Block> AMETHYST_BLOCKS = registerAllBlocks("amethyst",  new float[]{4f, 6f}, SoundType.STONE);*/
 
     public static final List<Item> COBALT_ITEMS = registerAllItems("cobalt", Tool.COBALT, Armor.COBALT,
-            new float[]{5f, -2f}, new float[]{3f, -2.8f}, new float[]{8f, -3.1f}, new float[]{0, 0f}, new float[]{4.5f, -3f},
+            new float[]{6f, -2f}, new float[]{3f, -2.8f}, new float[]{8f, -3.1f}, new float[]{0, 0f}, new float[]{4.5f, -3f},
             new Item.Properties().rarity(Rarity.UNCOMMON));
     public static final List<Block> COBALT_BLOCKS = registerAllBlocks("cobalt",  new float[]{6f, 8f}, SoundType.STONE,
             BlockBehaviour.Properties.of(),
             new Item.Properties().rarity(Rarity.UNCOMMON));
 
     public static final List<Item> INFERNAL_ITEMS = registerAllItems("infernal", Tool.INFERNAL, Armor.INFERNAL,
-            new float[]{7f, -2f}, new float[]{4f, -2.8f}, new float[]{8f, -3f}, new float[]{0, -1f}, new float[]{4.5f, -3f},
+            new float[]{7f, -1.8f}, new float[]{4f, -2.8f}, new float[]{8f, -3f}, new float[]{0, -1f}, new float[]{4.5f, -3f},
             new Item.Properties().fireResistant().rarity(Rarity.RARE));
-    public static final List<Block> INFERNAL_BLOCKS = registerAllBlocks("infernal",  new float[]{8f, 10f}, SoundType.STONE,
+    public static final List<Block> INFERNAL_BLOCKS = registerAllBlocks("infernal",  new float[]{8f, 10f}, SoundType.NETHER_ORE,
             BlockBehaviour.Properties.of(),
             new Item.Properties().fireResistant().rarity(Rarity.RARE));
 
     public static final List<Item> LEAD_ITEMS = registerAllItems("lead", Tool.LEAD, Armor.LEAD,
-            new float[]{5f, -2.4f}, new float[]{3f, -2.8f}, new float[]{8f, -3.1f}, new float[]{0, -1f}, new float[]{3.5f, -3f},
+            new float[]{4f, -2.6f}, new float[]{3f, -2.8f}, new float[]{8f, -3.1f}, new float[]{0, -1f}, new float[]{3.5f, -3f},
             new Item.Properties());
     public static final List<Block> LEAD_BLOCKS = registerAllBlocks("lead",  new float[]{4f, 6f}, SoundType.STONE,
             BlockBehaviour.Properties.of(),
             new Item.Properties());
 
     public static final List<Item> MYTHRIL_ITEMS = registerAllItems("mythril", Tool.MYTHRIL, Armor.MYTHRIL,
-            new float[]{7f, -1.4f}, new float[]{5f, -2.8f}, new float[]{9f, -3f}, new float[]{1, -2f}, new float[]{5.5f, -3f},
+            new float[]{8f, -2.4f}, new float[]{5f, -2.8f}, new float[]{9f, -3f}, new float[]{1, -2f}, new float[]{5.5f, -3f},
             new Item.Properties().rarity(Rarity.EPIC));
-    public static final List<Block> MYTHRIL_BLOCKS = registerAllBlocks("mythril",  new float[]{10f, 12f}, SoundType.STONE,
+    public static final List<Block> MYTHRIL_BLOCKS = registerAllBlocks("mythril",  new float[]{10f, 12f}, SoundType.DEEPSLATE,
             BlockBehaviour.Properties.of(),
             new Item.Properties().rarity(Rarity.EPIC));
 
     public static final List<Item> OBSIDIAN_ITEMS = registerAllItems("obsidian", Tool.OBSIDIAN, Armor.OBSIDIAN,
-            new float[]{7f, -3.2f}, new float[]{4f, -2.8f}, new float[]{8f, -3f}, new float[]{1, -1f}, new float[]{4.5f, -3f},
+            new float[]{9f, -3f}, new float[]{4f, -2.8f}, new float[]{8f, -3f}, new float[]{1, -1f}, new float[]{4.5f, -3f},
             new Item.Properties().fireResistant().rarity(Rarity.UNCOMMON));
     public static final Block OBSIDIAN_BLOCK = registerBlock("obsidian_block", Block::new, BlockBehaviour.Properties.of()
                     .requiresCorrectToolForDrops().strength(4f, 6f).sound(SoundType.METAL),
             new Item.Properties().fireResistant().rarity(Rarity.UNCOMMON));
 
     public static final List<Item> ORICHALCUM_ITEMS = registerAllItems("orichalcum", Tool.ORICHALCUM, Armor.ORICHALCUM,
-            new float[]{5f, -3f}, new float[]{4f, -2.8f}, new float[]{8f, -3f}, new float[]{0, -1f}, new float[]{4.5f, -3f},
+            new float[]{7.5f, -2f}, new float[]{4f, -2.8f}, new float[]{8f, -3f}, new float[]{0, -1f}, new float[]{4.5f, -3f},
             new Item.Properties().rarity(Rarity.RARE));
-    public static final List<Block> ORICHALCUM_BLOCKS = registerAllBlocks("orichalcum",  new float[]{8f, 10f}, SoundType.STONE,
+    public static final List<Block> ORICHALCUM_BLOCKS = registerAllBlocks("orichalcum",  new float[]{8f, 10f}, SoundType.DEEPSLATE,
             BlockBehaviour.Properties.of(),
             new Item.Properties().rarity(Rarity.RARE));
 
     public static final List<Item> PALLADIUM_ITEMS = registerAllItems("palladium", Tool.PALLADIUM, Armor.PALLADIUM,
-            new float[]{5f, -2f}, new float[]{3f, -2.8f}, new float[]{8f, -3.1f}, new float[]{1, 0f}, new float[]{4.5f, -3f},
+            new float[]{6f, -2.4f}, new float[]{3f, -2.8f}, new float[]{8f, -3.1f}, new float[]{1, 0f}, new float[]{4.5f, -3f},
             new Item.Properties().rarity(Rarity.UNCOMMON));
     public static final List<Block> PALLADIUM_BLOCKS = registerAllBlocks("palladium",  new float[]{6f, 8f}, SoundType.STONE,
             BlockBehaviour.Properties.of(),
             new Item.Properties().rarity(Rarity.UNCOMMON));
 
     public static final List<Item> PLATINUM_ITEMS = registerAllItems("platinum", Tool.PLATINUM, Armor.PLATINUM,
-            new float[]{5f, -2.2f}, new float[]{1f, -2.8f}, new float[]{6f, -3f}, new float[]{0f, -3f}, new float[]{1.5f, -3f},
+            new float[]{6f, -2.4f}, new float[]{1f, -2.8f}, new float[]{6f, -3f}, new float[]{0f, -3f}, new float[]{1.5f, -3f},
             new Item.Properties().rarity(Rarity.UNCOMMON));
     public static final List<Block> PLATINUM_BLOCKS = registerAllBlocks("platinum",  new float[]{6f, 8f}, SoundType.STONE,
             BlockBehaviour.Properties.of(),
@@ -122,14 +122,14 @@ public class ModItems {
             new Item.Properties());
 
     public static final List<Item> TITANIUM_ITEMS = registerAllItems("titanium", Tool.TITANIUM, Armor.TITANIUM,
-            new float[]{7f, -2.8f}, new float[]{4f, -2.8f}, new float[]{8f, -3f}, new float[]{1, -2f}, new float[]{5.5f, -3f},
+            new float[]{8f, -2.6f}, new float[]{4f, -2.8f}, new float[]{8f, -3f}, new float[]{1, -2f}, new float[]{5.5f, -3f},
             new Item.Properties().rarity(Rarity.EPIC));
-    public static final List<Block> TITANIUM_BLOCKS = registerAllBlocks("titanium",  new float[]{10f, 14f}, SoundType.STONE,
+    public static final List<Block> TITANIUM_BLOCKS = registerAllBlocks("titanium",  new float[]{10f, 14f}, SoundType.DEEPSLATE,
             BlockBehaviour.Properties.of(),
             new Item.Properties().rarity(Rarity.EPIC));
 
     public static final List<Item> TUNGSTEM_ITEMS = registerAllItems("tungsten", Tool.TUNGSTEM, Armor.TUNGSTEM,
-            new float[]{5f, -2f}, new float[]{3f, -2.8f}, new float[]{8f, -3.1f}, new float[]{0, -1f}, new float[]{3.5f, -3f},
+            new float[]{5f, -2.4f}, new float[]{3f, -2.8f}, new float[]{8f, -3.1f}, new float[]{0, -1f}, new float[]{3.5f, -3f},
             new Item.Properties());
     public static final List<Block> TUNGSTEM_BLOCKS = registerAllBlocks("tungsten",  new float[]{4f, 6f}, SoundType.STONE,
             BlockBehaviour.Properties.of(),
