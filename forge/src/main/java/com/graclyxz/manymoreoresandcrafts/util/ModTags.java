111191fa788845c42d1bf792fbd1940416fcfd55
Xavier Villavicencio <60786295+Graclyxz@users.noreply.github.com>
2025-03-31 22:07:00 -0500

    fabric and forge estable

--- forge/src/main/java/com/graclyxz/manymoreoresandcrafts/util/ModTags.java
+++ forge/src/main/java/com/graclyxz/manymoreoresandcrafts/util/ModTags.java
@@ -8,23 +8,23 @@ import net.minecraft.world.item.Item;
 public class ModTags {
     public interface Items {
         //TagKey<Item> ADAMANTITE_INGOT = createTag(new ResourceLocation("forge:ingots/adamantite"));
-        TagKey<Item> ADAMANTITE_INGOT = createTag(ResourceLocation.parse("forge:ingots/adamantite"));
-        TagKey<Item> COBALT_INGOT = createTag(ResourceLocation.parse("forge:ingots/cobalt"));
-        TagKey<Item> INFERNAL_INGOT = createTag(ResourceLocation.parse("forge:ingots/infernal"));
-        TagKey<Item> LEAD_INGOT = createTag(ResourceLocation.parse("forge:ingots/lead"));
-        TagKey<Item> MYTHRIL_INGOT = createTag(ResourceLocation.parse("forge:ingots/mythril"));
-        TagKey<Item> OBSIDIAN_INGOT = createTag(ResourceLocation.parse("forge:ingots/obsidian"));
-        TagKey<Item> ORICHALCUM_INGOT = createTag(ResourceLocation.parse("forge:ingots/orichalcum"));
-        TagKey<Item> PALLADIUM_INGOT = createTag(ResourceLocation.parse("forge:ingots/palladium"));
-        TagKey<Item> PLATINUM_INGOT = createTag(ResourceLocation.parse("forge:ingots/platinum"));
-        TagKey<Item> SILVER_INGOT = createTag(ResourceLocation.parse("forge:ingots/silver"));
-        TagKey<Item> TIN_INGOT = createTag(ResourceLocation.parse("forge:ingots/tin"));
-        TagKey<Item> TITANIUM_INGOT = createTag(ResourceLocation.parse("forge:ingots/titanium"));
-        TagKey<Item> TUNGSTEN_INGOT = createTag(ResourceLocation.parse("forge:ingots/tungsten"));
+        TagKey<Item> ADAMANTITE_INGOT = createTag(new ResourceLocation("forge:ingots/adamantite"));
+        TagKey<Item> COBALT_INGOT = createTag(new ResourceLocation("forge:ingots/cobalt"));
+        TagKey<Item> INFERNAL_INGOT = createTag(new ResourceLocation("forge:ingots/infernal"));
+        TagKey<Item> LEAD_INGOT = createTag(new ResourceLocation("forge:ingots/lead"));
+        TagKey<Item> MYTHRIL_INGOT = createTag(new ResourceLocation("forge:ingots/mythril"));
+        TagKey<Item> OBSIDIAN_INGOT = createTag(new ResourceLocation("forge:ingots/obsidian"));
+        TagKey<Item> ORICHALCUM_INGOT = createTag(new ResourceLocation("forge:ingots/orichalcum"));
+        TagKey<Item> PALLADIUM_INGOT = createTag(new ResourceLocation("forge:ingots/palladium"));
+        TagKey<Item> PLATINUM_INGOT = createTag(new ResourceLocation("forge:ingots/platinum"));
+        TagKey<Item> SILVER_INGOT = createTag(new ResourceLocation("forge:ingots/silver"));
+        TagKey<Item> TIN_INGOT = createTag(new ResourceLocation("forge:ingots/tin"));
+        TagKey<Item> TITANIUM_INGOT = createTag(new ResourceLocation("forge:ingots/titanium"));
+        TagKey<Item> TUNGSTEN_INGOT = createTag(new ResourceLocation("forge:ingots/tungsten"));
 
-        TagKey<Item> COPPER = createTag(ResourceLocation.parse("forge:ingots/copper"));
-        TagKey<Item> AMETHYST_SHARD = createTag(ResourceLocation.parse("forge:items/amethyst_shard"));
-        TagKey<Item> EMERALD = createTag(ResourceLocation.parse("forge:items/emerald"));
+        TagKey<Item> COPPER = createTag(new ResourceLocation("forge:ingots/copper"));
+        TagKey<Item> AMETHYST_SHARD = createTag(new ResourceLocation("forge:items/amethyst_shard"));
+        TagKey<Item> EMERALD = createTag(new ResourceLocation("forge:items/emerald"));
 
         private static TagKey<Item> createTag(ResourceLocation name) {
             return TagKey.create(Registries.ITEM, name);
